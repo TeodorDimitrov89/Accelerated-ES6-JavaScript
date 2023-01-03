95f865edea45d4fe25b786656cbacd9016b8ddc5
TeodorDimitrov <teodordimitrov908@gmail.com>
2023-01-03 16:30:52 +0200

    generators and iterators

--- 05.iterators-generators/index.js
+++ 05.iterators-generators/index.js
@@ -1,3 +1,6 @@
+/* 
+  Iterators
+*/
 // let array = [1, 2, 3];
 
 // array[Symbol.iterator] = function () {
@@ -17,25 +20,61 @@
 //   console.log(element);
 // }
 
-let person = {
-  name: "Teo",
-  hobbies: ["Sport", "Cooking"],
-  [Symbol.iterator]: function () {
-    let i = 0;
-    let hobbies = this.hobbies;
-    return {
-      next: function () {
-        let value = hobbies[i];
-        i++;
-        return {
-          done: i > hobbies.length ? true : false,
-          value: value,
-        };
-      },
-    };
-  },
-};
+// let person = {
+//   name: "Teo",
+//   hobbies: ["Sport", "Cooking"],
+//   [Symbol.iterator]: function () {
+//     let i = 0;
+//     let hobbies = this.hobbies;
+//     return {
+//       next: function () {
+//         let value = hobbies[i];
+//         i++;
+//         return {
+//           done: i > hobbies.length ? true : false,
+//           value: value,
+//         };
+//       },
+//     };
+//   },
+// };
 
-for (let hobby of person) {
-  console.log(hobby);
+// for (let hobby of person) {
+//   console.log(hobby);
+// }
+
+/* 
+  Generators
+*/
+
+//example 1
+
+// let obj = {
+//   [Symbol.iterator]: gen,
+// };
+
+// function* gen() {
+//   yield 1;
+//   yield 2;
+// }
+
+// for (let element of obj) {
+//   console.log(element, "object Element");
+// }
+
+//example 2
+function* gen(end) {
+  for (let i = 0; i < end; i++) {
+    try {
+      yield i;
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
+
+let it = gen(2);
+console.log(it.next());
+console.log(it.next());
+console.log(it.throw("An error occured!"));
+console.log(it.next());
