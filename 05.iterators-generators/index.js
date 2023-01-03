79dd272f92acb8b9f61a964703b83b8ef262442f
TeodorDimitrov <teodordimitrov908@gmail.com>
2023-01-03 15:49:53 +0200

    creating a custom iterable object

--- 05.iterators-generators/index.js
+++ 05.iterators-generators/index.js
@@ -1,18 +1,41 @@
-let array = [1, 2, 3];
+// let array = [1, 2, 3];
 
-array[Symbol.iterator] = function () {
-  let nextValue = 7;
-  return {
-    next: function () {
-      nextValue++;
-      return {
-        done: nextValue > 15 ? true : false,
-        value: nextValue,
-      };
-    },
-  };
+// array[Symbol.iterator] = function () {
+//   let nextValue = 7;
+//   return {
+//     next: function () {
+//       nextValue++;
+//       return {
+//         done: nextValue > 15 ? true : false,
+//         value: nextValue,
+//       };
+//     },
+//   };
+// };
+
+// for (let element of array) {
+//   console.log(element);
+// }
+
+let person = {
+  name: "Teo",
+  hobbies: ["Sport", "Cooking"],
+  [Symbol.iterator]: function () {
+    let i = 0;
+    let hobbies = this.hobbies;
+    return {
+      next: function () {
+        let value = hobbies[i];
+        i++;
+        return {
+          done: i > hobbies.length ? true : false,
+          value: value,
+        };
+      },
+    };
+  },
 };
 
-for (let element of array) {
-  console.log(element);
+for (let hobby of person) {
+  console.log(hobby);
 }
